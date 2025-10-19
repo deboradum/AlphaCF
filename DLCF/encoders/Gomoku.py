e5f3761c128a9e4276dabdc757057c45ff46c488
deboradum <pvwpvw@protonmail.com>
2025-10-19 17:33:44 +0200

    more gomoku features

--- DLCF/encoders/Gomoku.py
+++ DLCF/encoders/Gomoku.py
@@ -17,10 +17,16 @@ FEATURE_OFFSETS = {
     'black_pieces_t1': 2,
     'white_pieces_t1': 3,
 
-    # Other features
     'legal_moves': 4,
-    'black_to_move': 5,
-    'white_to_move': 6,
+    'player_to_move': 5,  # 1s if Black to move, 0s if White
+
+    # Threat-based features
+    'player_fours': 6,       # Moves that create 5-in-a-row (win)
+    'opponent_fours': 7,     # Moves that block an opponent's 5-in-a-row
+    'player_threes': 8,      # Moves that create an "open three" (_XXX_)
+    'opponent_threes': 9,    # Moves that block an opponent's "open three"
+    'player_threats_in_two': 10, # Moves that create >= 2 threats (fours or threes)
+    'opponent_threats_in_two': 11, # Moves that block >= 2 opponent threats
 }
 
 class GomokuEncoder(Encoder):
@@ -29,8 +35,43 @@ class GomokuEncoder(Encoder):
         self.board_height, self.board_width = board_size
         self.num_planes = len(FEATURE_OFFSETS.keys())
 
+        self._directions = [(0, 1), (1, 0), (1, 1), (1, -1)] # H, V, Diag\, Diag/
+
     def name(self):
         return 'Gomoku'
+
+    @staticmethod
+    def _is_in_bounds(r, c, board_height, board_width):
+        return 0 <= r < board_height and 0 <= c < board_width
+
+    def _find_threats(self, r: int, c: int,
+                        player_plane: torch.Tensor,
+                        opponent_plane: torch.Tensor) -> Tuple[int, int]:
+        num_fours = 0
+        num_threes = 0
+
+        for dr, dc in self._directions:
+            line_str = ""
+            for i in range(-4, 5): # 9-cell line centered on the move
+                cr, cc = r + i * dr, c + i * dc
+
+                if not self._is_in_bounds(cr, cc, self.board_height, self.board_width):
+                    line_str += "B" # Border
+                elif i == 0:
+                    line_str += "P" # The hypothetical move
+                elif player_plane[cr, cc] == 1:
+                    line_str += "P" # Player's stone
+                elif opponent_plane[cr, cc] == 1:
+                    line_str += "O" # Opponent's stone
+                else:
+                    line_str += "E" # Empty
+
+            if "PPPPP" in line_str:
+                num_fours += 1
+            if "EPPPPE" in line_str:
+                num_threes += 1
+
+        return num_fours, num_threes
 
     def encode(self, game_states: List[GameStateTemplate]):
         batch_size = len(game_states)
@@ -51,15 +92,22 @@ class GomokuEncoder(Encoder):
         # Legal move indices
         b_idx_legal, r_idx_legal, c_idx_legal = [], [], []
 
+        # Threat indices
+        b_idx_p4, r_idx_p4, c_idx_p4 = [], [], []
+        b_idx_o4, r_idx_o4, c_idx_o4 = [], [], []
+        b_idx_p3, r_idx_p3, c_idx_p3 = [], [], []
+        b_idx_o3, r_idx_o3, c_idx_o3 = [], [], []
+        b_idx_p2t, r_idx_p2t, c_idx_p2t = [], [], []
+        b_idx_o2t, r_idx_o2t, c_idx_o2t = [], [], []
+
         for i, game_state in enumerate(game_states):
             # Player to move
             if game_state.next_player == Player.black:
-                board_tensors[i, FEATURE_OFFSETS['black_to_move']] = 1
-            else:
-                board_tensors[i, FEATURE_OFFSETS['white_to_move']] = 1
+                board_tensors[i, FEATURE_OFFSETS['player_to_move']] = 1
 
             # Legal Moves
-            for m in game_state.legal_moves():
+            legal_moves_list = game_state.legal_moves()
+            for m in legal_moves_list:
                 b_idx_legal.append(i)
                 r_idx_legal.append(m.point.row - 1)
                 c_idx_legal.append(m.point.col - 1)
@@ -92,6 +140,36 @@ class GomokuEncoder(Encoder):
                             r_idx_white_t1.append(row - 1)
                             c_idx_white_t1.append(col - 1)
 
+            player = game_state.next_player
+            opponent = Player.white if player == Player.black else Player.black
+
+            pieces_planes = self.get_pieces_planes(game_state)
+            player_plane = pieces_planes[BLACK_IDX] if player == Player.black else pieces_planes[WHITE_IDX]
+            opponent_plane = pieces_planes[WHITE_IDX] if player == Player.black else pieces_planes[BLACK_IDX]
+
+            for m in legal_moves_list:
+                r, c = m.point.row - 1, m.point.col - 1
+
+                # Check threats for player
+                p_fours, p_threes = self._find_threats(r, c, player_plane, opponent_plane)
+
+                if p_fours > 0:
+                    b_idx_p4.append(i); r_idx_p4.append(r); c_idx_p4.append(c)
+                if p_threes > 0:
+                    b_idx_p3.append(i); r_idx_p3.append(r); c_idx_p3.append(c)
+                if p_fours + p_threes >= 2:
+                    b_idx_p2t.append(i); r_idx_p2t.append(r); c_idx_p2t.append(c)
+
+                # Check threats for opponent
+                o_fours, o_threes = self._find_threats(r, c, opponent_plane, player_plane)
+
+                if o_fours > 0:
+                    b_idx_o4.append(i); r_idx_o4.append(r); c_idx_o4.append(c)
+                if o_threes > 0:
+                    b_idx_o3.append(i); r_idx_o3.append(r); c_idx_o3.append(c)
+                if o_fours + o_threes >= 2:
+                    b_idx_o2t.append(i); r_idx_o2t.append(r); c_idx_o2t.append(c)
+
         # Assign current pieces
         board_tensors[b_idx_black_t0, FEATURE_OFFSETS['black_pieces_t0'], r_idx_black_t0, c_idx_black_t0] = 1
         board_tensors[b_idx_white_t0, FEATURE_OFFSETS['white_pieces_t0'], r_idx_white_t0, c_idx_white_t0] = 1
@@ -100,8 +178,16 @@ class GomokuEncoder(Encoder):
         board_tensors[b_idx_black_t1, FEATURE_OFFSETS['black_pieces_t1'], r_idx_black_t1, c_idx_black_t1] = 1
         board_tensors[b_idx_white_t1, FEATURE_OFFSETS['white_pieces_t1'], r_idx_white_t1, c_idx_white_t1] = 1
 
-        # legal moves
+        # Assign legal moves
         board_tensors[b_idx_legal, FEATURE_OFFSETS['legal_moves'], r_idx_legal, c_idx_legal] = 1
+
+        # Assign threat planes
+        board_tensors[b_idx_p4, FEATURE_OFFSETS['player_fours'], r_idx_p4, c_idx_p4] = 1
+        board_tensors[b_idx_o4, FEATURE_OFFSETS['opponent_fours'], r_idx_o4, c_idx_o4] = 1
+        board_tensors[b_idx_p3, FEATURE_OFFSETS['player_threes'], r_idx_p3, c_idx_p3] = 1
+        board_tensors[b_idx_o3, FEATURE_OFFSETS['opponent_threes'], r_idx_o3, c_idx_o3] = 1
+        board_tensors[b_idx_p2t, FEATURE_OFFSETS['player_threats_in_two'], r_idx_p2t, c_idx_p2t] = 1
+        board_tensors[b_idx_o2t, FEATURE_OFFSETS['opponent_threats_in_two'], r_idx_o2t, c_idx_o2t] = 1
 
         return board_tensors
 
